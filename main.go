7e67253a23a5666866ab00c9d6ef8619864f265b
maxdemian-tw <110931353+maxdemian-tw@users.noreply.github.com>
2022-12-20 19:40:23 -0300

    implem handler code for posting

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 type Recipe struct {
+	ID           string    `json:"string"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -14,7 +17,26 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+// Recipes is a temporary storage while a persistent storage is not implemented
+var recipes []Recipe
+
+// init will be executed in the application loading
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
+// newRecipeHandler will store the recipes using the post method
 func newRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
 
 }
 
