e139627496a7ea4525bb606369366fa98403fb21
maxdemian-tw <110931353+maxdemian-tw@users.noreply.github.com>
2022-12-21 16:20:50 -0300

    list recipes endpoint

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 type Recipe struct {
+	ID           string    `json:"string"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -14,7 +19,38 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+// Recipes is a temporary storage while a persistent storage is not implemented
+var recipes []Recipe
+
+// init will be executed in the application loading
+func init() {
+	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
+}
+
+// newRecipeHandler will store the recipes using the post method
+func newRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+
+}
+
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, recipes)
+}
+
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", newRecipeHandler)
+	router.GET("/recipes", ListRecipesHandler)
 	router.Run()
 }
